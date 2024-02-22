3e388407600235cdf1c93cbd8d894a1a1b0fb7d3
olinb <olin.bourquin@gmail.com>
2024-02-22 13:51:08 +0100

    Add observer and Subject classes

---
+++ src/observer/Subject.java
@@ -0,0 +1,19 @@
+package observer;
+
+import java.util.*;
+
+public abstract class Subject {
+    private List<Observer> observers = new ArrayList<>();
+
+    public void attach(Observer o) {
+        observers.add(o);
+    }
+
+    public void detach(Observer o) {
+        observers.remove(o);
+    }
+
+    public void notifyObservers() {
+        observers.forEach(Observer::update);
+    }
+}
