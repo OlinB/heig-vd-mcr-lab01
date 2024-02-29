1a1eb474c4166efc28b513322f2fca0712fcb74c
Jonas Troeltsch <jonas.troeltsch@heig-vd.ch>
2024-02-29 11:53:28 +0100

    basic control panel

--- src/ui/DialManager.java
+++ src/ui/DialManager.java
@@ -4,8 +4,13 @@ import ui.Dial;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class DialManager extends JFrame {
+
+    private static final String[] BUTTONS_DIAL = {"démarrer", "arrêter", "réinitialiser", "cadran romain", "cadran arabe", "numérique"};
+    private static final String[] BUTTONS_ALL = {"cadran romain", "cadran arabe", "numérique"};
 
     public DialManager(int nbDials) {
         super("Panneau de contrôle");
@@ -17,10 +22,28 @@ public class DialManager extends JFrame {
             panel.add(label);
 
             // buttons
+            for (String text : BUTTONS_DIAL) {
+                JButton button = new JButton(text);
+                button.addActionListener(e -> System.out.println("Button " + text + " clicked"));
+                panel.add(button);
+            }
 
             add(panel);
 
+
         }
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+        // label
+        JLabel label = new JLabel("Tous les chronos");
+        panel.add(label);
+
+        for (String text : BUTTONS_ALL) {
+            JButton button = new JButton(text);
+            button.addActionListener(e -> System.out.println("Button " + text + " clicked"));
+            panel.add(button);
+        }
+        add(panel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         setVisible(true);
