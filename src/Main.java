a5ecd31238be925807608d738504f9e77c59b08d
Jonas Troeltsch <jonas.troeltsch@heig-vd.ch>
2024-03-12 12:46:12 +0100

    add header in main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 import chrono.Chrono;
 import ui.DialManager;
-
+/**
+ * @author Olin Bourquin
+ * @author Jonas Troeltsch
+ * @version 1.0
+ * @since 2024-02-22
+ */
 public class Main {
     public static void main(String[] args) {
         DialManager dm = new DialManager(3);
